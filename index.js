86da7a811ac58f41b658729f6c860712429cf828
Clyde Bueckert <clydeb@criticalmass.com>
2023-02-27 13:35:51 -0700

    Refactoring and Adding Accessibility Features
    allow tabbing and keyboard interaction with nav items, refactoring code

--- index.js
+++ index.js
@@ -1,43 +1,69 @@
 import citiesData from './navigation.json' assert {type: 'json'};
 
-const {cities} = citiesData;
 
+let selected;
 const navDiv = document.querySelector('.nav_content');
 let navMargin = parseInt(window.getComputedStyle(navDiv).marginLeft.replace('px', ''));//todo long and also regex
-
-//todo instantiate function and refactor
-//todo make it so the nav is tabbable and accessible
+const underline = document.querySelector('.underline');
 //todo run in other browsers  get the webkit transition properties
 //todo mobile views
-const navContainer = document.querySelector('.nav_container');
-cities.forEach((city, index) => {
-    const a = document.createElement('a');
-    a.innerText = city.label;
-    if (index === 0) a.classList.add('active');
-    a.classList.add('nav_item');
-    navContainer.appendChild(a);
-});
+
+window.onload = () => {
+    createAndAppendElements();
+    instantiateUnderline();
+}
+
 window.onresize = () => {
-    navMargin = parseInt(window.getComputedStyle(navDiv).marginLeft.replace('px', ''));
-    const {width, x} = selected.getBoundingClientRect();
+    recalculateUnderlinePosition();
+}
+
+const createAndAppendElements = () => {
+    const {cities} = citiesData;
+    const navContainer = document.querySelector('.nav_container');
+    cities.forEach((city, index) => {
+        const a = document.createElement('a');
+        a.innerText = city.label;
+        if (index === 0) a.classList.add('active');
+        a.classList.add('nav_item');
+        a.setAttribute('tabindex', '0');
+        navContainer.appendChild(a);
+    });
+}
+
+const instantiateUnderline = () => {
+    const navItems = document.querySelectorAll('.nav_item');
+    selected = navItems[0];
+    calculateUnderlinePosition(selected);
+    underline.style.display = 'block';
+    navItems.forEach((a) => {
+        a.addEventListener('click', () => {
+            handleClick(a);
+        });
+        a.addEventListener("keydown", (event) => {
+            if (event.keyCode === 13) {
+                handleClick(a);
+            }
+        })
+    });
+}
+
+const handleClick = (newElement) => {
+    selected.classList.toggle('active');
+    selected = newElement;
+    selected.classList.toggle('active');
+    calculateUnderlinePosition(selected);
+}
+
+
+const calculateUnderlinePosition = (focusedElement) => {
+    const {width, x} = focusedElement.getBoundingClientRect(); //TODO DOES THIS WORK IN ALL BROWSERS
     underline.style.width = `${width}px`;
     underline.style.marginLeft = `${x - navMargin}px`;
-}
-const underline = document.querySelector('.underline');
-const navItems = document.querySelectorAll('.nav_item');
-let selected = navItems[0];
-const {width, x} = selected.getBoundingClientRect();
-underline.style.width = `${width}px`;
-underline.style.marginLeft = `${x - navMargin}px`;
-underline.style.display = 'block';
+    underline.style.display = 'block';
 
-navItems.forEach((a) => {
-    a.addEventListener('click', () => {
-        selected.classList.toggle('active');
-        selected = a;
-        selected.classList.toggle('active');
-        const {width, x} = selected.getBoundingClientRect();
-        underline.style.width = `${width}px`;
-        underline.style.marginLeft = `${x - navMargin}px`;
-    })
-})
+}
+
+const recalculateUnderlinePosition = () => {
+    navMargin = parseInt(window.getComputedStyle(navDiv).marginLeft.replace('px', ''));
+    calculateUnderlinePosition(selected);
+}
