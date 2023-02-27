c9a02e0289b1f6e60e17f85dfe1b1428f6317980
Clyde Bueckert <clydeb@criticalmass.com>
2023-02-27 12:52:17 -0700

    Implement Sliding Bar
    Refactor ul to use a tags, add first implementation of sliding bar

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Developer_Exercise_Clyde
 My implementation of the Retail_InStore_Developer_Exercise_NoLibraries 02/27/2023 
+
+https://blog.bitsrc.io/how-to-import-json-file-as-a-module-e4965295a7b3
+https://codepen.io/noonii/pen/MEepKE
--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 import citiesData from './navigation.json' assert {type: 'json'};
 
 const {cities} = citiesData;
-//https://blog.bitsrc.io/how-to-import-json-file-as-a-module-e4965295a7b3
-console.log(citiesData);
-console.log(cities);
-console.log('done');
 
-const navUL = document.querySelector('.nav_ul');
-cities.forEach((city) => {
-    const li = document.createElement('li');
+const navDiv = document.querySelector('.nav_content');
+let navMargin = parseInt(window.getComputedStyle(navDiv).marginLeft.replace('px', ''));//todo long and also regex
+
+//todo instantiate function and refactor
+//todo make it so the nav is tabbable and accessible
+//todo run in other browsers  get the webkit transition properties
+//todo mobile views
+const navContainer = document.querySelector('.nav_container');
+cities.forEach((city, index) => {
     const a = document.createElement('a');
     a.innerText = city.label;
+    if (index === 0) a.classList.add('active');
     a.classList.add('nav_item');
-    li.appendChild(a);
-    navUL.appendChild(li);
+    navContainer.appendChild(a);
+});
+window.onresize = () => {
+    navMargin = parseInt(window.getComputedStyle(navDiv).marginLeft.replace('px', ''));
+    const {width, x} = selected.getBoundingClientRect();
+    underline.style.width = `${width}px`;
+    underline.style.marginLeft = `${x - navMargin}px`;
+}
+const underline = document.querySelector('.underline');
+const navItems = document.querySelectorAll('.nav_item');
+let selected = navItems[0];
+const {width, x} = selected.getBoundingClientRect();
+underline.style.width = `${width}px`;
+underline.style.marginLeft = `${x - navMargin}px`;
+underline.style.display = 'block';
+
+navItems.forEach((a) => {
+    a.addEventListener('click', () => {
+        selected.classList.toggle('active');
+        selected = a;
+        selected.classList.toggle('active');
+        const {width, x} = selected.getBoundingClientRect();
+        underline.style.width = `${width}px`;
+        underline.style.marginLeft = `${x - navMargin}px`;
+    })
 })
