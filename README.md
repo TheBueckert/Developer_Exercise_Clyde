baca12aca211b29ce3e75903dc8a19f960a0876b
Clyde Bueckert <clydeb@criticalmass.com>
2023-02-27 17:47:25 -0700

    Implement Hamburger Menu, Refactor JSON Import
    add hamburger menu and views for different screen sizes, refactor code, refactor JSON data import to work in Safari

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 My implementation of the Retail_InStore_Developer_Exercise_NoLibraries 02/27/2023 
 
 https://blog.bitsrc.io/how-to-import-json-file-as-a-module-e4965295a7b3
+https://github.com/eslint/eslint/discussions/15305
+https://stackoverflow.com/questions/54617844/using-filereader-to-read-a-json-file
+https://stackoverflow.com/questions/48073151/read-local-json-file-into-variable
 https://codepen.io/noonii/pen/MEepKE
+Hamburger menu:
+https://codepen.io/erikterwan/pen/EVzeRP
+https://alvarotrigo.com/blog/hamburger-menu-css/
--- index.js
+++ index.js
@@ -1,32 +1,46 @@
-import citiesData from './navigation.json' assert {type: 'json'};
-
-
-let selected;
+let citiesData;
+let selected, selectedMobile, navMargin;
+let mainNav = document.querySelector('.main_nav');
 const navDiv = document.querySelector('.nav_content');
-let navMargin = parseInt(window.getComputedStyle(navDiv).marginLeft.replace('px', ''));//todo long and also regex
 const underline = document.querySelector('.underline');
 //todo run in other browsers  get the webkit transition properties
 //todo mobile views
 
 window.onload = () => {
+    loadJSON((json) => {
+        citiesData = json;
+    });
+    calculateNavMargin();
     createAndAppendElements();
     instantiateUnderline();
+    instantiateMobileMenu();
 }
 
 window.onresize = () => {
-    recalculateUnderlinePosition();
+    calculateNavMargin();
+    calculateUnderlinePosition(selected);
 }
 
 const createAndAppendElements = () => {
     const {cities} = citiesData;
     const navContainer = document.querySelector('.nav_container');
+    const mobileContainer = document.querySelector('.menu_items');
     cities.forEach((city, index) => {
+        // Desktop Nav Items
         const a = document.createElement('a');
         a.innerText = city.label;
         if (index === 0) a.classList.add('active');
         a.classList.add('nav_item');
         a.setAttribute('tabindex', '0');
         navContainer.appendChild(a);
+
+        //Hamburger Menu Items
+        const aMobile = document.createElement('a');
+        aMobile.innerText = city.label;
+        aMobile.classList.add('nav_item_mobile');
+        aMobile.setAttribute('tabindex', '0');
+        if (index === 0) aMobile.classList.add('active');
+        mobileContainer.appendChild(aMobile);
     });
 }
 
@@ -43,15 +57,56 @@ const instantiateUnderline = () => {
             if (event.keyCode === 13) {
                 handleClick(a);
             }
-        })
+        });
     });
 }
 
-const handleClick = (newElement) => {
+const instantiateMobileMenu = () => {
+    const navItemsMobile = document.querySelectorAll('.nav_item_mobile');
+    const selectedCityText = document.querySelector('.selected_city_mobile');
+    selectedMobile = navItemsMobile[0];
+    selectedCityText.innerText = selectedMobile.innerHTML;
+    navItemsMobile.forEach((a) => {
+        a.addEventListener('click', () => {
+            handleClick(a);
+            selectedCityText.innerText = a.innerText;
+        });
+        a.addEventListener("keydown", (event) => {
+            if (event.keyCode === 13) {
+                handleClick(a);
+                selectedCityText.innerText = a.innerText;
+            }
+        });
+    });
+    document.querySelector('.hamburger_lines').addEventListener('keydown', (event) => {
+        if (event.keyCode === 13)
+            document.getElementById("checkbox").checked = !document.getElementById("checkbox").checked
+    });
+
+}
+
+const handleClick = (selectedElement) => {
+    const navItems = [...document.querySelectorAll('.nav_item')];
+    const navItemsMobile = [...document.querySelectorAll('.nav_item_mobile')];
+    const newElementDesktop = navItems.filter((item) => item.innerText === selectedElement.innerText);
+    const newElementMobile = navItemsMobile.filter((item) => item.innerText === selectedElement.innerText);
+
     selected.classList.toggle('active');
-    selected = newElement;
+    selected = newElementDesktop[0];
     selected.classList.toggle('active');
+
+    selectedMobile.classList.toggle('active');
+    selectedMobile = newElementMobile[0];
+    selectedMobile.classList.toggle('active');
+
     calculateUnderlinePosition(selected);
+    document.getElementById("checkbox").checked = false;
+}
+
+const calculateNavMargin = () => {
+    let val1 = parseInt(window.getComputedStyle(mainNav).marginLeft.replace('px', ''));//todo long and also regex
+    let val2 = parseInt(window.getComputedStyle(navDiv).marginLeft.replace('px', ''));//todo long and also regex
+    navMargin = val1 + val2;
 }
 
 
@@ -63,7 +118,14 @@ const calculateUnderlinePosition = (focusedElement) => {
 
 }
 
-const recalculateUnderlinePosition = () => {
-    navMargin = parseInt(window.getComputedStyle(navDiv).marginLeft.replace('px', ''));
-    calculateUnderlinePosition(selected);
+
+const loadJSON = (callback) => {
+    const xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', './navigation.json', false);
+    xobj.onreadystatechange = () => {
+        if (xobj.readyState === 4 && xobj.status === 200)
+            callback(JSON.parse(xobj.responseText));
+    };
+    xobj.send(null);
 }
